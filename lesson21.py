fff32118a819cd2e579aefe5c0783c1430711b76
varsenikharutyunian <varsikharutyunyan72@gmail.com>
2023-04-25 14:20:20 +0400

    Added lesson21.py

--- lesson21.py
+++ lesson21.py
@@ -2,13 +2,21 @@
 string="100"
 integer = int(string)
 print( integer)
-"""2. Create a function that takes a boolean variable flag and returns it as a string."""
+"""2. Create a function that takes a boolean variable flag and returns it as a string.
+Examples
+True ➞ "True"
+False ➞ "False"""
 # bool = "True"
 # print(bool)     @ sxal e
 # bool= "False"
 # print(bool)
 """3. Write a function that returns the string
-"something" joined with a space " " and the given argument a."""
+"something" joined with a space " " and the given argument a.
+Examples
+"is better than nothing" ➞ "something is better than nothing"
+"Bob Jane" ➞ "something Bob Jane"
+"something" ➞ "something something"""
+
 name = input("Enter name - >")
 x="something is better than nothing"
 y="something Bob Jane"
@@ -16,19 +24,39 @@ z="something"
 result= (x* (name == " is better than nothing " ) +  y*(name == " Bob Jane")+(z*(name == "something")))
 print(result)
 """4. Luke Skywalker has family and friends. Help him remind them who is who. Given a string with a name,
-return the relation of that person to Luke."""
+return the relation of that person to Luke.
+Person  Relation
+Darth Vader father
+Leia    sister
+Han brother in law
+R2D2    droid
+Examples
+"Darth Vader" ➞ "Luke, I am your father."
+"Leia" ➞ "Luke, I am your sister."
+"Han" ➞ "Luke, I am your brother in law."""
+
 name = input("Enter name - >")
 x =  "Luke, I am your father."
 y = "Luke, I am your sister."
 z= "Luke, I am your brother in law."
 print(x * (name == "Darth Vader") + y*(name=="Leia")+ z*(name=="Han"))
 """5.Create a function that takes a string and returns the number (count)
- of vowels contained within it."""
+ of vowels contained within it.
+ Examples
+"Celebration" ➞ 5
+"Palm" ➞ 1
+"Prediction" ➞ 4
+Notes
+a, e, i, o, u are considered vowels (not y)."""
  
 name = input("Enter name - >")
 print(name.count("a")+name.count("e")+name.count("i")+name.count("o")+name.count("u"))
 """6.Create a function that returns True if a given
- inequality expression is correct and False otherwise."""     # sxal e
+ inequality expression is correct and False otherwise.
+ Examples
+"3 < 7 < 11" ➞ True
+"13 > 44 > 33 > 1" ➞ False
+"1 < 2 < 6 < 9 > 3" ➞ True"""     
 # x=3<7<11 
 y=13 > 44 > 33 > 1
 # z=1 < 2 < 6 < 9 > 3
@@ -38,7 +66,12 @@ eval("print(y)")
 # print(x )
 # print(y)
 # print(z)
-"""7.Create a function that replaces all the vowels in a string with a specified character."""
+"""7.Create a function that replaces all the vowels in a string with a specified character.
+Examples
+"the aardvark", "#" ➞ "th# ##rdv#rk"
+"minnie mouse", "?" ➞ "m?nn?? m??s?"
+"shakespeare", "*" ➞ "sh*k*sp**r*"""
+
 input_str = input("enter string - >")
 print(input_str.replace("a","#" ) .replace("e","#"))
 
@@ -60,7 +93,10 @@ print(len(y)-4) * "*"+ y[-4:]
 
 
 """"9.Create a function that takes a string (will be a person's first and last name)
-and returns a string with the first and last name swapped."""
+and returns a string with the first and last name swapped.Examples
+"Donald Trump" ➞ "Trump Donald"
+"Rosie O'Donnell" ➞ "O'Donnell Rosie"
+"Seymour Butts" ➞ "Butts Seymour"""
 
 name=input("enter name_surname ->")
 name=name.split()                        
