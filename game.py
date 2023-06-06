24c38ba01b56e33ee5522e79f44e168e46935cfc
varsenikharutyunian <varsikharutyunyan72@gmail.com>
2023-06-06 23:27:31 +0400

    added game.py

--- game.py
+++ game.py
@@ -3,7 +3,7 @@ from Board import BoardClass
 class Game:
     def __init__(self) -> None:
         self.board=BoardClass
-        def test(self)
+    def play(self):
 self.board.change_board_state(2,'x')   
 self.board.change_boars_state(4,'x') 
 self.board.print_board() 
